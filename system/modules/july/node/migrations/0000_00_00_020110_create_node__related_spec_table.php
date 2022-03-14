904d4f266c9f09888e7a2fbb5c85676c302ec02e
刘 <877368032@qq.com>
2022-03-14 08:51:09 +0800

    修改.env文件名称

---
+++ system/modules/july/node/migrations/0000_00_00_020110_create_node__related_spec_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nodes', function (Blueprint $table) {
+            $table->id();
+
+            // 节点类型
+            $table->string('entity_id');
+
+            // 标题
+            $table->string('related_spec');
+
+            // 源语言
+            $table->string('langcode', 12);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('node__related_spec');
+    }
+}
