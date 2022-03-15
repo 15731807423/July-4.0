dabc662918ab1eb054503ab0df513b786f123c86
liu <l15731807423@gmail.com>
2022-03-15 17:41:42 +0800

    设置不上传的文件

---
+++ system/storage/settings/language.php
@@ -0,0 +1,29 @@
+<?php
+
+return array (
+  'lang.multiple' => true,
+  'lang.available' => 
+  array (
+    'en' => 
+    array (
+      'accessible' => true,
+      'translatable' => true,
+      'name' => '英语',
+      'icon' => '/images/favicon.ico',
+    ),
+    'zh' => 
+    array (
+      'accessible' => true,
+      'translatable' => true,
+      'name' => '简体中文',
+    ),
+    'aa' => 
+    array (
+      'translatable' => true,
+      'accessible' => true,
+      'name' => '日语',
+    ),
+  ),
+  'lang.content' => 'en',
+  'lang.frontend' => 'en',
+);
