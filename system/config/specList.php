d5ca5c2ca33e6f2c37340809a4dfb23800f36cda
刘 <877368032@qq.com>
2022-04-02 11:14:27 +0800

    修改规格列表配置的默认值

--- system/config/specList.php
+++ system/config/specList.php
@@ -3,10 +3,63 @@
 return [
 	'model' => 'static',
 	'cuttingSymbol' => ',',
-	'dataEmptyText' => NULL,
+	'dataEmptyText' => null,
 	'static' => [
+		'search' => [
+			'status' => false,
+			'default' => null,
+			'caseSensitive' => null,
+			'class' => null,
+			'inputConfig' => [
+				'onInput' => null,
+				'onChange' => null,
+				'class' => null,
+				'componentConfig' => null
+			],
+			'buttonConfig' => [
+				'status' => false,
+				'text' => null,
+				'class' => null,
+				'componentConfig' => null
+			]
+		],
 		'screen' => [
-			'groupCountType' => []
+			'status' => false,
+			'userStatus' => false,
+			'clearText' => null,
+			'selectedClass' => null,
+			'countStatus' => false,
+			'groupCountType' => [],
+			'type' => 1,
+			'nullHidden' => false,
+			'class' => null,
+			'allClass' => null
+		],
+		'selector' => [
+			'class' => null,
+			'list' => [
+				'table' => [
+					'text' => null
+				],
+				'list' => [
+					'text' => null
+				]
+			],
+			'config' => [
+				'componentConfig' => null
+			]
+		],
+		'pagination' => [
+			'class' => null,
+			'pageSize' => null,
+			'currentPage' => null,
+			'componentConfig' => null
+		],
+		'loading' => [
+			'status' => false,
+			'config' => [
+				'componentConfig' => null
+			]
 		]
 	],
 ];
