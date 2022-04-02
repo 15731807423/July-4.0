3c657d24f3ece44635b204eb19d14a3bea183f60
liu <l15731807423@gmail.com>
2022-04-02 17:05:14 +0800

    初始化框架的脚本

--- run.bat
+++ run.bat
@@ -1,8 +1,1 @@
-cd system
-rename .env.production .env
-composer install
-cd ../front-vue
-npm install
-cd ../
-rm run.bat
-pause
+cd system && rename .env.production .env && composer install && cd ../front-vue && npm install && rm ../run.bat
