d2d1a35a8473c6faf3c68c1a3160735469dd8198
刘 <877368032@qq.com>
2022-04-07 14:48:04 +0800

    后台-组件-规格-编辑里新增的配置存放数据库字段

--- system/modules/specs/migrations/0000_00_00_030001_create_specs_table.php
+++ system/modules/specs/migrations/0000_00_00_030001_create_specs_table.php
@@ -23,6 +23,24 @@ class CreateSpecsTable extends Migration
             // 描述
             $table->string('description', 255)->nullable();
 
+            // 展示表格
+            $table->boolean('table_status')->default(true);
+
+            // 默认排序字段
+            $table->string('default_sort_field')->nullable();
+
+            // 默认排序方式
+            $table->string('default_sort_mode')->nullable();
+
+            // 表格的配置信息
+            $table->string('table_config', 255)->nullable();
+
+            // 展示列表
+            $table->boolean('list_status')->default(true);
+
+            // 列表的布局
+            $table->string('list_item', 255)->nullable();
+
             $table->timestamps();
         });
     }
