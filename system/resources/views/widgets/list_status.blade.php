3983a9f0f2fed1c75ea33a5b70f877a4fbaa6d6c
刘 <877368032@qq.com>
2022-04-07 14:57:36 +0800

    后台-组件-规格-编辑页面新增配置信息

---
+++ system/resources/views/widgets/list_status.blade.php
@@ -0,0 +1,3 @@
+<el-form-item label="{{ $_label ?? '展示列表' }}" prop="id" size="small" class="has-helptext">
+    <el-switch v-model="{{ $_model }}.list_status" active-text="开启" inactive-text="关闭"></el-switch>
+</el-form-item>
---
+++ system/resources/views/widgets/table_config.blade.php
@@ -0,0 +1,3 @@
+<el-form-item v-if="{{ $_model }}.table_status" label="{{ $_label ?? '表格的配置信息' }}" prop="id" size="small" class="has-helptext">
+    <el-input name="table_config" v-model="{{ $_model }}.table_config" type="textarea" rows="{{ $_rows ?? 5 }}" maxlength="255" show-word-limit></el-input>
+</el-form-item>
