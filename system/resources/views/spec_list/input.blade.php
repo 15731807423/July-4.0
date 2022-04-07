efee52f1d5bbdfee7421b6a9a2be89ccac269514
liu <l15731807423@gmail.com>
2022-04-07 14:45:51 +0800

    后台-配置-规格列表配置信息的默认值显示在input的placeholder里

--- system/resources/views/spec_list/input.blade.php
+++ system/resources/views/spec_list/input.blade.php
@@ -2,7 +2,7 @@
     <el-tooltip slot="label" popper-class="jc-twig-output" effect="dark" content="{!! $data['tips'] !!}" placement="right">
         <span>{{ $data['label'] }}</span>
     </el-tooltip>
-    <el-input v-model="settings['{{ $index }}']"></el-input>
+    <el-input v-model="settings['{{ $index }}']" placeholder="{{ isset($data['default']) ? $data['default'] : '' }}"></el-input>
     @if ($data['description'])
     <span class="jc-form-item-help"><i class="el-icon-info"></i> {{ $data['description'] }}</span>
     @endif
