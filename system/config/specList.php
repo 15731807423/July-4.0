5b423cdb39f576fcb284d19c6571371a3bab04a0
liu <l15731807423@gmail.com>
2022-04-11 08:23:27 +0800

    修改规格列表配置信息

--- system/config/specList.php
+++ system/config/specList.php
@@ -5,74 +5,72 @@ return [
 	'cuttingSymbol' => '',
 	'dataEmptyText' => null,
 	'sortCaseSensitive' => false,
-	'static' => [
-		'search' => [
-			'status' => false,
-			'default' => null,
-			'caseSensitive' => null,
+	'search' => [
+		'status' => false,
+		'default' => null,
+		'caseSensitive' => null,
+		'class' => null,
+		'inputConfig' => [
+			'onInput' => null,
+			'onChange' => null,
 			'class' => null,
-			'inputConfig' => [
-				'onInput' => null,
-				'onChange' => null,
-				'class' => null,
-				'componentConfig' => null
-			],
-			'buttonConfig' => [
-				'status' => false,
-				'text' => null,
-				'class' => null,
-				'componentConfig' => null
-			]
-		],
-		'screen' => [
-			'status' => false,
-			'userStatus' => false,
-			'clearText' => null,
-			'selectedClass' => null,
-			'countStatus' => false,
-			'groupCountType' => [],
-			'type' => '1',
-			'nullHidden' => false,
-			'class' => null,
-			'allClass' => null
-		],
-		'selector' => [
-			'class' => null,
-			'list' => [
-				'table' => [
-					'text' => null
-				],
-				'list' => [
-					'text' => null
-				]
-			],
-			'config' => [
-				'componentConfig' => null
-			]
-		],
-		'pagination' => [
-			'class' => null,
-			'pageSize' => null,
-			'currentPage' => null,
 			'componentConfig' => null
 		],
-		'loading' => [
+		'buttonConfig' => [
 			'status' => false,
-			'config' => [
-				'componentConfig' => null
+			'text' => null,
+			'class' => null,
+			'componentConfig' => null
+		]
+	],
+	'screen' => [
+		'status' => false,
+		'userStatus' => false,
+		'clearText' => null,
+		'selectedClass' => null,
+		'countStatus' => false,
+		'groupCountType' => [],
+		'type' => '1',
+		'nullHidden' => false,
+		'class' => null,
+		'allClass' => null
+	],
+	'selector' => [
+		'class' => null,
+		'list' => [
+			'table' => [
+				'text' => null
+			],
+			'list' => [
+				'text' => null
 			]
 		],
-		'specAll' => [
-			'specConfig' => null,
-			'status' => false,
-			'title' => 'Category',
-			'sortable' => false,
-			'searchable' => false,
-			'screenable' => false,
-			'screenType' => '1',
-			'screenDefault' => null,
-			'screenConfig' => null,
-			'screenGroupConfig' => null,
+		'config' => [
+			'componentConfig' => null
 		]
+	],
+	'pagination' => [
+		'class' => null,
+		'pageSize' => null,
+		'currentPage' => null,
+		'componentConfig' => null
+	],
+	'loading' => [
+		'status' => false,
+		'config' => [
+			'componentConfig' => null
+		]
+	],
+	'specAll' => [
+		'specConfig' => null,
+		'status' => false,
+		'title' => 'Category',
+		'sortable' => false,
+		'searchable' => false,
+		'screenable' => false,
+		'screenType' => '1',
+		'screenDefault' => null,
+		'screenConfig' => null,
+		'screenGroupConfig' => null,
 	]
 ];
