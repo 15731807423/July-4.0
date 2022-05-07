328e7afe45505420baa1fd149298bd8e7aafe1e4
刘 <877368032@qq.com>
2022-05-07 10:45:27 +0800

    国家顶级域名和对应语言的代码

---
+++ system/language/code.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+	'en'	=> 'en',
+	'cn'	=> 'zh-Hans',
+	'jp'	=> 'ja',
+	'de'	=> 'de',
+	'it'	=> 'it',
+	'es'	=> 'es',
+	'pt'	=> 'pt',
+	'fr'	=> 'fr',
+	'kr'	=> 'ko',
+	'ru'	=> 'ru',
+];
