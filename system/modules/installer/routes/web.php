782f93585e3fada12c86931fefc0680b46973aaa
liu <l15731807423@gmail.com>
2022-06-06 08:10:05 +0800

    添加修改数据库页面的路由

--- system/modules/installer/routes/web.php
+++ system/modules/installer/routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Installer\Controllers\UpdateController;
 use Installer\Controllers\InstallController;
 
+// 安装
 Route::get('install', [InstallController::class, 'home'])->name('install.home');
 Route::post('install', [InstallController::class, 'install'])->name('install.install');
 Route::post('install/migrate', [InstallController::class, 'migrate'])->name('install.migrate');
+
+// 切换数据库
+Route::get('update/db', [UpdateController::class, 'dbHome'])->name('update.db');
+Route::post('update/db', [UpdateController::class, 'dbUpdate'])->name('update.db.update');
