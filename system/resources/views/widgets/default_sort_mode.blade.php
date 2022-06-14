2e4a32d275e57005f1c8a1eecb238eff29652562
刘 <877368032@qq.com>
2022-06-14 08:24:12 +0800

    修改规格编辑页面的字段验证

--- system/resources/views/widgets/default_sort_mode.blade.php
+++ system/resources/views/widgets/default_sort_mode.blade.php
@@ -1,4 +1,4 @@
-<el-form-item v-if="{{ $_model }}.table_status" label="{{ $_label ?? '默认排序方式' }}" prop="id" size="small" class="has-helptext">
+<el-form-item v-if="{{ $_model }}.table_status" label="{{ $_label ?? '默认排序方式' }}" size="small" class="has-helptext">
     <el-radio-group v-model="{{ $_model }}.default_sort_mode">
         <el-radio label="asc">正序</el-radio>
         <el-radio label="desc">倒序</el-radio>
--- system/resources/views/widgets/list_item.blade.php
+++ system/resources/views/widgets/list_item.blade.php
@@ -1,3 +1,3 @@
-<el-form-item v-if="{{ $_model }}.list_status" label="{{ $_label ?? '列表的布局' }}" prop="id" size="small" class="has-helptext">
+<el-form-item v-if="{{ $_model }}.list_status" label="{{ $_label ?? '列表的布局' }}" size="small" class="has-helptext">
     <el-input name="list_item" v-model="{{ $_model }}.list_item" type="textarea" rows="{{ $_rows ?? 5 }}" maxlength="255" show-word-limit></el-input>
 </el-form-item>
