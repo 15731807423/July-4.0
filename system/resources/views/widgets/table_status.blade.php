6f7042a7eabc465838fd8d4873afa05693ba9b84
liu <l15731807423@gmail.com>
2022-06-14 08:24:12 +0800

    修改规格编辑页面的字段验证

--- system/resources/views/widgets/table_status.blade.php
+++ system/resources/views/widgets/table_status.blade.php
@@ -1,3 +1,3 @@
-<el-form-item label="{{ $_label ?? '展示表格' }}" prop="id" size="small" class="has-helptext">
+<el-form-item label="{{ $_label ?? '展示表格' }}" size="small" class="has-helptext">
     <el-switch v-model="{{ $_model }}.table_status" active-text="开启" inactive-text="关闭"></el-switch>
 </el-form-item>
