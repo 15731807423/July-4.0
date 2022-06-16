8dbe85e18b67a6d0d405d28c2b30c01f7d259ff0
liu <l15731807423@gmail.com>
2022-06-16 16:03:08 +0800

    根目录下的front-vue内容放到themes/backend/vue下

--- system/modules/july/node/views/node/index.blade.php
+++ system/modules/july/node/views/node/index.blade.php
@@ -141,8 +141,8 @@
 @endsection
 
 @section('script')
-<script src="/themes/backend/vue/js/c-pagination.js"></script>
-<link rel="stylesheet" type="text/css" href="/themes/backend/vue/css/c-pagination.css">
+<script src="/themes/backend/js/c-pagination.js"></script>
+<link rel="stylesheet" type="text/css" href="/themes/backend/css/c-pagination.css">
 <script type="text/javascript" src="/themes/backend/js/translate.js"></script>
 <script>
     Vue.component('c-pagination', cPagination)
