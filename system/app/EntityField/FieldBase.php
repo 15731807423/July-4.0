e61b1f3b54b5587f9db27067a2a8b213638f71d5
刘 <45228445+15731807423@users.noreply.github.com>
2022-08-19 10:35:42 +0800

    Update FieldBase.php
    序列化

--- system/app/EntityField/FieldBase.php
+++ system/app/EntityField/FieldBase.php
@@ -373,7 +373,7 @@ abstract class FieldBase extends ModelBase implements TranslatableInterface
             // }
 
             $meta = $this->attributesToArray();
-            is_string($meta['field_meta']) && $meta['field_meta'] = unserialize($meta['field_meta']);
+            isset($meta['field_meta']) && is_string($meta['field_meta']) && $meta['field_meta'] = unserialize($meta['field_meta']);
             $meta = array_merge($meta, $meta['field_meta'] ?? []);
             unset($meta['field_meta']);
 
