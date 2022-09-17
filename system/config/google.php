b0687d44bc059aa60e4c3203be0538b38f2ffeba
刘 <877368032@qq.com>
2022-09-17 08:59:12 +0800

    谷歌模块的配置项

---
+++ system/config/google.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+	'web' => [
+		'client_id'						=> '724605947131-58l1t8r05ogjjlhisu524ne3t252p0i2.apps.googleusercontent.com',
+		'project_id'					=> 'excellent-math-359101',
+		'auth_uri'						=> 'https://accounts.google.com/o/oauth2/auth',
+		'token_uri'						=> 'https://oauth2.googleapis.com/token',
+		'auth_provider_x509_cert_url'	=> 'https://www.googleapis.com/oauth2/v1/certs',
+		'client_secret'					=> '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',
+		'redirect_uris'					=> [
+			'https://www.test.vip/manage/google/oauth2callback'
+		],
+		'refresh_token' => '1//0eHK7unF10ditCgYIARAAGA4SNwF-L9IrW0I4EzDkMt6IxhAjyZCGEMVlCZtAYq9avKc_ppCmmkiUVn-QmldeMdj6MNEkWaKR_2c',
+		'developer_token' => 'C4kI2P2JyMu3BNWZnwHD1Q',
+		'login_customer_id' => '7052590237'
+	]
+];
