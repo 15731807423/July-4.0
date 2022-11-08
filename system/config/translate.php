63995af3ebdac81200ad3f5bfc9a9d183811e976
liu <l15731807423@gmail.com>
2022-11-08 09:07:48 +0800

    翻译设置的内容

--- system/config/translate.php
+++ system/config/translate.php
@@ -1,7 +1,20 @@
 <?php
 
 return [
-	'fields'			=> '',
-	'text'				=> '',
-	'replace'			=> '',
+	'fields'	=> '[\'url\',\'meta_canonical\',\'image_src\',\'timeout\']',
+	'text'		=> '',
+	'replace'	=> '',
+
+	'tool'		=> 'azure',
+	'mode'		=> 'direct',
+	'list'		=> [
+		'alibabacloud'	=> [
+			'name'			=> '阿里云',
+			'mode'			=> 'task'
+		],
+		'azure'			=> [
+			'name'			=> '微软',
+			'mode'			=> 'task'
+		]
+	]
 ];
