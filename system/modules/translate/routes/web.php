bcb92d8d03d3a35e467f57f036eca7202bb73d85
刘 <877368032@qq.com>
2022-11-08 09:11:29 +0800

    翻译接口的路由

--- system/modules/translate/routes/web.php
+++ system/modules/translate/routes/web.php
@@ -1,21 +1,41 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Translate\Controllers;
+use Translate\Controllers\TaskController as Task;
+use Translate\Controllers\DirectController as Direct;
 
 Route::prefix(config('app.management_prefix', 'admin'))
 ->name('manage.')
 ->middleware(['admin','auth'])
 ->group(function() {
+    // 直接翻译获取结果
     // 批量翻译
-    Route::post('translate/batch', [Controllers\TranslateController::class, 'batch'])->name('translate.batch');
+    Route::post('translate/direct/batch', [Direct::class, 'batch'])->name('translate.direct.batch');
 
     // 翻译一个页面
-    Route::post('translate/page', [Controllers\TranslateController::class, 'page'])->name('translate.page');
+    Route::post('translate/direct/page', [Direct::class, 'page'])->name('translate.direct.page');
 
     // 创建模板并翻译
-    Route::post('translate/tpl/{code}', [Controllers\TranslateController::class, 'tpl'])->name('translate.tpl');
+    Route::post('translate/direct/tpl', [Direct::class, 'tpl'])->name('translate.direct.tpl');
 
-    // 获取翻译结果
-    Route::post('translate/result', [Controllers\TranslateController::class, 'result'])->name('translate.result');
+
+
+    // 创建任务获取结果
+    // 批量翻译
+    Route::post('translate/task/batch', [Task::class, 'batch'])->name('translate.task.batch');
+
+    // 翻译一个页面
+    Route::post('translate/task/page', [Task::class, 'page'])->name('translate.task.page');
+
+    // 创建模板并翻译
+    Route::post('translate/task/tpl', [Task::class, 'tpl'])->name('translate.task.tpl');
+
+    // 批量翻译 获取结果
+    Route::post('translate/task/batch/result', [Task::class, 'batchResult'])->name('translate.task.batch.result');
+
+    // 翻译一个页面 获取结果
+    Route::post('translate/task/page/result', [Task::class, 'pageResult'])->name('translate.task.page.result');
+
+    // 创建模板并翻译 获取结果
+    Route::post('translate/task/tpl/result', [Task::class, 'tplResult'])->name('translate.task.tpl.result');
 });
