6b943eddc27a1df1deb68da7d4e156f9aabb57a9
liu <l15731807423@gmail.com>
2022-11-11 15:26:53 +0800

    添加腾讯云翻译

--- system/config/translate.php
+++ system/config/translate.php
@@ -12,6 +12,10 @@ return [
 			'name'			=> '阿里云',
 			'mode'			=> 'task'
 		],
+		'tencentcloud'	=> [
+			'name'			=> '腾讯云',
+			'mode'			=> 'task'
+		],
 		'azure'			=> [
 			'name'			=> '微软',
 			'mode'			=> 'task'
