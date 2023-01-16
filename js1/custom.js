5ee1ca7c9643aaead0c66bbb45d19e76c294681e
刘 <877368032@qq.com>
2023-01-16 17:41:16 +0800

    /js重命名为/js1并更新js文件

---
+++ js1/custom.js
@@ -0,0 +1,46 @@
+﻿LazyScript.load('owl.carousel', 'lazyload', 'lightbox', 'tabs', 'swiper', 'sendMailOpenPdf', 'member', 'scrollPopup', function(global) {
+    // 配置点击按钮提交表单打开pdf
+    new SendMailOpenPdf({
+        button: '.send-mail-open-pdf',
+        form: '.send-mail-open-pdf-form'
+    });
+
+    new Member({
+        registerForm: '#register',
+        registerContainer: '.register',
+        loginForm: '#login',
+        loginContainer: '.login',
+        logoutButton: '#logout',
+        userContainer: '.user',
+        username: '.username'
+    });
+
+    //  首页弹窗
+    const popup = new ScrollPopup({
+        selector: '.popup',
+        form: '.popup .form form',
+        result: '.popup .result',
+        submit: data => {
+            $('.popup .result .message').html(data.message);
+            $('.popup .form').hide();
+            $('.popup .result').show();
+
+            if (data.status) {
+                $('.popup .result .back').hide();
+            } else {
+
+            }
+        },
+        slideDown: 0,
+        closeButton: '.popup .form .close, .popup .result .close',
+        animationDuration: 1,
+        mask: {
+            close: true
+        }
+    });
+
+    $('.popup .result .back').click(e => {
+        $('.popup .result').hide();
+        $('.popup .form').show();
+    });
+});
