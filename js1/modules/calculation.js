7a2ab0935c6fe377d0f53d977857623b705291c9
liu <l15731807423@gmail.com>
2023-02-06 11:22:02 +0800

    添加新功能

--- js1/modules/calculation.js
+++ js1/modules/calculation.js
@@ -12,11 +12,45 @@ LazyScript.load('jquery', function (global) {
         data.resultCondition    = parseValue(data.resultCondition, 'string');
         data.reset              = parseValue(data.reset, 'string');
 
-        // 存放所有参数 提交按钮 重置按钮
-        const param = {}, submit = data.submit ? $(data.submit) : null, reset = data.reset ? $(data.reset) : null;
+        // 存放所有参数 表单的值 绑定错误信息的id 提交按钮 重置按钮
+        const param = {}, form = {}, bind = [], submit = data.submit ? $(data.submit) : null, reset = data.reset ? $(data.reset) : null;
 
-        // 注册全部参数
-        register();
+        //  把所有表单放进对象里 表单的值定义全局变量
+        for (let i = 0; i < data.param.length; i++) {
+            let item = data.param[i], element = $(item.selector);
+            param[item.name] = {
+                selector: item.selector,
+                element: element,
+                number: item.number,
+                decimal: item.decimal,
+                type: item.type,
+                range: item.range,
+                sync: item.sync,
+                statusError: false,
+                statusDisabled: false,
+                statusHidden: false,
+                errorMessage: []
+            };
+
+            form[item.name] = null;
+        }
+
+        // 循环处理每个参数
+        for (let key in param) {
+            // 如果是文本框 监听输入事件 如果是下拉菜单等 监听改变事件
+            // param[key].type == 'input' ? listenInput(key) : param[key].element.change(change);
+            param[key].type == 'input' ? listenInput(key) : listenOther(key);
+        }
+
+        // 处理绑定错误信息
+        for (var i = 0; i < data.error.length; i++) {
+            if (data.error[i].bind === null) continue;
+            if (data.error[i].bind === i) continue;
+
+            let array = [i, data.error[i].bind], json = JSON.stringify(array.sort((a, b) => { return a - b; }));
+
+            bind.indexOf(json) === -1 && bind.push(array);
+        }
 
         // 如果有提交按钮 点击按钮时触发计算
         submit && submit.click(calculationCheck);
@@ -39,7 +73,10 @@ LazyScript.load('jquery', function (global) {
             data.gather ? gather() : dispersed();
 
             // 重新注册全部参数
-            register();
+            for (let key in param) {
+                param[key].element = $(param[key].selector);
+                param[key].type == 'input' ? listenInput(key) : listenOther(key);
+            }
 
             // 值发生了变化
             change();
@@ -68,6 +105,10 @@ LazyScript.load('jquery', function (global) {
                     if (type == 'boolean') return value;
                     break;
 
+                case 'number':
+                    if (type == 'number') return value;
+                    break;
+
                 case 'param':
                     if (type != 'array') return [];
                     for (var i = 0; i < value.length; i++) {
@@ -78,6 +119,7 @@ LazyScript.load('jquery', function (global) {
                             number      : parseValue(value[i].number, 'boolean', true),
                             range       : parseValue(value[i].range, 'param[].range'),
                             decimal     : parseValue(value[i].decimal, 'param[].decimal', 0),
+                            sync        : parseValue(value[i].sync, 'boolean', false),
                         };
                     }
                     return value;
@@ -102,6 +144,7 @@ LazyScript.load('jquery', function (global) {
                                 name        : parseValue(value[i].name, 'string'),
                                 condition   : parseValue(value[i].condition, 'string'),
                                 message     : parseValue(value[i].message, 'string'),
+                                bind        : parseValue(value[i].bind, 'number'),
                                 false       : parseValue(value[i].false, 'function'),
                                 true        : parseValue(value[i].true, 'function')
                             };
@@ -168,37 +211,17 @@ LazyScript.load('jquery', function (global) {
             return defaultValue === undefined ? null : defaultValue;
         }
 
-        // 注册全部参数
-        function register() {
-            //  把所有表单放进对象里 表单的值定义全局变量
-            for (let i = 0; i < data.param.length; i++) {
-                let item = data.param[i], element = $(item.selector);
-                param[item.name] = {
-                    selector: item.selector,
-                    element: element,
-                    number: item.number,
-                    decimal: item.decimal,
-                    type: item.type,
-                    range: item.range,
-                    statusError: false,
-                    statusDisabled: false,
-                    statusHidden: false,
-                    errorMessage: []
-                };
-
-                eval(`var ${item.name}`);
-            }
-
-            // 循环处理每个参数
-            for (let key in param) {
-                // 如果是文本框 监听输入事件 如果是下拉菜单等 监听改变事件
-                param[key].type == 'input' ? listenInput(key) : param[key].element.change(change);
-            }
-        }
-
         // 监听文本框的输入事件 判断小数长度等
         function listenInput(name) {
             param[name].element.bind('input propertychange', () => listenInputEvent(name));
+        }
+
+        // 监听其他元素的修改事件
+        function listenOther(name) {
+            param[name].element.change(function () {
+                param[name].sync && param[name].element.val($(this).val());
+                change(name);
+            });
         }
 
         // 输入事件
@@ -231,13 +254,13 @@ LazyScript.load('jquery', function (global) {
             }
 
             // 执行
-            change();
+            change(name);
         }
 
         // 有值发生变化了就要触发
-        function change() {
+        function change(name) {
             refresh();
-            check();
+            check(name);
 
             // 如果没有提交按钮 计算结果
             submit || calculationCheck();
@@ -255,31 +278,51 @@ LazyScript.load('jquery', function (global) {
                     } else {
                         $(item.selector + ':checked').each((index, element) => value.push(element.value));
                     }
-                    eval(`${key} = ` + JSON.stringify(value));
+                    form[key] = JSON.stringify(value);
                 } else if (item.type == 'radio') {
                     // 单选表单 获取选中的值 如果没选 值为null 如果必须是数字转成数字
                     var value = $(item.selector + ':checked').val();
                     value === undefined && (value = null);
                     item.number && (value = parseFloat(value));
-                    eval(`${key} = ` + ($.type(value) == 'string' ? `"${value}"` : value));
+                    form[key] = value;
                 } else {
                     // 文本框和下拉菜单 获取值 如果必须是数字转成数字
                     var value = item.element.val();
                     item.number && (value = value.length == 0 ? null : parseFloat(value));
-                    eval(`${key} = ` + ($.type(value) == 'string' ? `"${value}"` : value));
+                    form[key] = value;
                 }
             }
         }
 
         // 检查下面三项
-        function check() {
-            checkError();
-            checkDisabled();
-            checkHidden();
+        function check(name) {
+            checkError(name);
+            checkDisabled(name);
+            checkHidden(name);
         }
 
         // 检查有没有错误
-        function checkError() {
+        function checkError(name) {
+            var ignore = [];
+
+            if (name) {
+                // 循环每个绑定的组
+                for (var i = 0; i < bind.length; i++) {
+                    // 根据当前修改的参数 获取组里哪个是被修改的 哪个是对比的
+                    let update, compare;
+                    data.error[bind[i][0]].name == name && (update = bind[i][0], compare = bind[i][1]);
+                    data.error[bind[i][1]].name == name && (update = bind[i][1], compare = bind[i][0]);
+
+                    if (update !== null && compare !== null) {
+                        // 如果有 记录对比的项的错误信息被忽略 并移除对比参数的错误信息
+                        ignore.push(compare);
+                        param[data.error[compare].name].statusError = false;
+                        param[data.error[compare].name].errorMessage = [];
+                        data.error[compare].false();
+                    }
+                }
+            }
+
             // 如果错误信息一起展示
             if (data.gather) {
                 // 先移除全部错误
@@ -293,11 +336,12 @@ LazyScript.load('jquery', function (global) {
 
                 // 检查每个错误
                 for (let i = 0; i < data.error.length; i++) {
-                    let value = eval(data.error[i].name), info = param[data.error[i].name];
+                    // 忽略的错误跳过
+                    if (ignore.indexOf(i) !== -1) continue;
 
                     // 判断文本框的值是否错误
-                    if (info.type == 'input') {
-                        value !== null && eval(data.error[i].condition) && (
+                    if (param[data.error[i].name].type == 'input') {
+                        run(`${data.error[i].name} !== null && (${data.error[i].condition})`) && (
                             message[data.error[i].name]
                             ? message[data.error[i].name].push(data.error[i].message)
                             : message[data.error[i].name] = [data.error[i].message]
@@ -317,14 +361,15 @@ LazyScript.load('jquery', function (global) {
 
                 // 检查每个错误
                 for (let i = 0; i < data.error.length; i++) {
-                    let value = eval(data.error[i].name), info = param[data.error[i].name];
+                    // 忽略的错误跳过
+                    if (ignore.indexOf(i) !== -1) continue;
 
                     // 判断文本框的值是否错误
-                    if (info.type == 'input') {
+                    if (param[data.error[i].name].type == 'input') {
                         // 已经出错了就不再判断
                         if (list.indexOf(data.error[i].name) !== -1) continue;
 
-                        if (value !== null && eval(data.error[i].condition)) {
+                        if (run(`${data.error[i].name} !== null && (${data.error[i].condition})`)) {
                             // 如果有值且执行结果为真且该参数还没出现错误 记录下来 因为只显示第一个错误 执行真函数 参数设置为错误 设置错误信息
                             list.push(data.error[i].name);
                             // data.error[i].true(data.error[i].message);
@@ -351,7 +396,7 @@ LazyScript.load('jquery', function (global) {
 
             for (var i = 0; i < data.disabled.length; i++) {
                 if (param[data.disabled[i].name].type == 'input') {
-                    if (eval(data.disabled[i].condition)) {
+                    if (run(data.disabled[i].condition)) {
                         // 结果为真应该禁用 但是先判断当前是否禁用 如果是不执行任何操作 否则执行真函数 修改状态 清空值并记录
                         if (param[data.disabled[i].name].statusDisabled == false) {
                             data.disabled[i].true();
@@ -377,7 +422,7 @@ LazyScript.load('jquery', function (global) {
         function checkHidden() {
             for (var i = 0; i < data.hidden.length; i++) {
                 // 执行表达式 如果为真 执行真函数并记录 否则 执行假函数并移除记录
-                eval(data.hidden[i].condition)
+                run(data.hidden[i].condition)
                 ? (data.hidden[i].true(), param[data.hidden[i].name].statusHidden = true)
                 : (data.hidden[i].false(), param[data.hidden[i].name].statusHidden = false);
             }
@@ -437,12 +482,12 @@ LazyScript.load('jquery', function (global) {
 
             if (Object.keys(errorMessage).length > 0) {
                 // 有错误信息则执行 清空结果内容 返回假表示有错
-                eval('(' + data.gather + ')(' + JSON.stringify(errorMessage) + ')');
+                run('(' + data.gather + ')(' + JSON.stringify(errorMessage) + ')');
                 data.resultClear && result(null);
                 return false;
             } else {
                 // 没错误信息则传null 表示没错 用来隐藏元素 返回真表示没错
-                eval('(' + data.gather + ')(null)');
+                run('(' + data.gather + ')(null)');
                 return true;
             }
         }
@@ -461,7 +506,7 @@ LazyScript.load('jquery', function (global) {
             }
 
             // 如果上面的判断都通过了 执行传进来的条件
-            status && (status = eval('!!(' + data.resultCondition + ')'));
+            status && (status = run('!!(' + data.resultCondition + ')'));
 
             // 显示错误信息
             // 这个函数calculationCheck可能是input触发也可能是提交按钮触发
@@ -478,13 +523,13 @@ LazyScript.load('jquery', function (global) {
         // 计算结果
         function calculation() {
             for (let i = 0; i < data.result.length; i++) {
-                let item = data.reset[i], type = $.type(item.calculation);
+                let item = data.result[i], type = $.type(item.calculation);
 
                 // 字符串时执行表达式 函数时执行函数 否则为空
                 if (type == 'string') {
-                    var value = eval(item.calculation);
+                    var value = run(item.calculation);
                 } else if (type == 'function') {
-                    var value = eval('(' + item.calculation + ')()');
+                    var value = run('(' + item.calculation + ')()');
                 } else {
                     var value = '';
                 }
@@ -517,6 +562,15 @@ LazyScript.load('jquery', function (global) {
         // 判断是不是数字
         function number(value) {
             return value === parseFloat(value).toString()
+        }
+
+        // 执行一段代码或一个函数
+        function run(expression) {
+            for (let key in form) {
+                eval(`var ${key} = ${$.type(form[key]) == 'string' ? `"${form[key]}"` : form[key]};`);
+            }
+
+            return eval(expression);
         }
     }
 
