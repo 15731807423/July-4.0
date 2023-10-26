bc94d6a52d001991aa375f1f3e55e8ee054e968a
unknown <877368032@qq.com>
2023-10-26 16:22:02 +0800

    导出放到菜单里

---
+++ system/app/Support/Settings/Export.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Support\Settings;
+
+class Export extends SettingGroupBase
+{
+    /**
+     * 配置组名称
+     *
+     * @var string
+     */
+    protected $name = 'export';
+
+    /**
+     * 配置组标题
+     *
+     * @var string
+     */
+    protected $title = '导出数据';
+
+    /**
+     * 配置项
+     *
+     * @var array
+     */
+    protected $items = [
+
+    ];
+}
