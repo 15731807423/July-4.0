1d3e4ed256095d389dba77c9628b595727c3042b
unknown <877368032@qq.com>
2023-11-08 16:45:39 +0800

    后台设置发件人

--- system/app/Support/Settings/SiteInformation.php
+++ system/app/Support/Settings/SiteInformation.php
@@ -36,9 +36,21 @@ class SiteInformation extends SettingGroupBase
             'description' => '网站所服务的主体（企业或个人）的名称',
         ],
 
+        'mail.from.name' => [
+            'key' => 'mail.from.name',
+            'label' => '发件人名称',
+            'description' => '邮件发件人名称',
+        ],
+
+        'mail.from.address' => [
+            'key' => 'mail.from.address',
+            'label' => '发件人地址',
+            'description' => '邮件发件人地址',
+        ],
+
         'site.mails' => [
             'key' => 'site.mails',
-            'label' => '邮箱',
+            'label' => '收件人',
             'description' => '联系表单的默认接收邮箱',
         ],
     ];
